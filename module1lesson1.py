f5b9963466b6d90084ce9e37485e87026781ae27
jdanos07 <jdanos07@gmail.com>
2024-09-07 17:17:01 -0400

    Module 1, Lessons 1 & 2

---
+++ module1lesson1.py
@@ -0,0 +1,15 @@
+My_reason = '''I am learning to code to
+spend more time with my family. 
+I currently spend more time then I would like commuting to work.
+Also, to break into a new career field. Something I see as more creative,
+but the only tool is a computer.'''
+print  (My_reason)
+
+#I was instructed to make a comment on the following arithmatic function
+
+a= 2+2
+print (a)
+
+b=5*5*5*5*5*5
+print (b)
+#that was for S&G's
---
+++ module1lesson2.py
@@ -0,0 +1,8 @@
+a = 6
+print (a)
+
+a = 94
+print (a)
+
+b = 94; c = 30; d = 97
+print (a, b, c, d)
