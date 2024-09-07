0969d480f3c7692d3a5679bd90e9bb7c2f806932
jdanos07 <jdanos07@gmail.com>
2024-09-07 17:22:21 -0400

    cleaned up comments & code structure

--- module1lesson2.py
+++ module1lesson2.py
@@ -1,8 +1,13 @@
-a = 6 #defined variable a per lesson instruction
+a = 6 
+    #defined variable a per lesson instruction
 print (a)
 
-a = 94 #redefined variable a per lesson instruction
+a = 94 
+    #redefined variable a per lesson instruction
 print (a)
 
-b = 94; c = 30; d = 97 #defined multiple, numerous, variables in a single line per lesson instruction
+b = 94; c = 30; d = 97 
+    #defined multiple, numerous, variables in a single line per lesson instruction
 print (a, b, c, d)
+
+#pushed lesson 1 & 2 simultaniously to GH to practice that process.
