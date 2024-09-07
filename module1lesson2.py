94b4e31b97cb8836a8a2e54972b3b98429b5f4c7
jdanos07 <jdanos07@gmail.com>
2024-09-07 17:20:32 -0400

    added comments to lines

--- module1lesson2.py
+++ module1lesson2.py
@@ -1,8 +1,8 @@
-a = 6
+a = 6 #defined variable a per lesson instruction
 print (a)
 
-a = 94
+a = 94 #redefined variable a per lesson instruction
 print (a)
 
-b = 94; c = 30; d = 97
+b = 94; c = 30; d = 97 #defined multiple, numerous, variables in a single line per lesson instruction
 print (a, b, c, d)
